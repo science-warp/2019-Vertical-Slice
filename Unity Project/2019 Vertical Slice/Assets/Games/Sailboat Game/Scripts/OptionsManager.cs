bbf71e3f8cd8420012a3d3efcd31577cc2482cd2
Erin's Laptop <nern.strudel@gmail.com>
2019-07-31 17:33:08 -0400

    small updates, beginning of implementing unit settings

--- Unity Project/2019 Vertical Slice/Assets/Games/Sailboat Game/Scripts/OptionsManager.cs
+++ Unity Project/2019 Vertical Slice/Assets/Games/Sailboat Game/Scripts/OptionsManager.cs
@@ -6,18 +6,45 @@ public class OptionsManager : MonoBehaviour
 {
 
 
+    public enum speedUnit { MPH, KTS, MPS};
 
+    public enum distanceUnit { M, FT};
 
+    public speedUnit currentSpeedUnit = speedUnit.MPH;
+    public distanceUnit currentDistanceUnit = distanceUnit.FT;
 
     // Start is called before the first frame update
     void Start()
     {
+        //this thing sticks around the whole time, unless one already exists. Yay singletons
+
+        if(GameObject.FindObjectsOfType<OptionsManager>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+
         DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /// <summary>
+    /// Sets the speed units to be used.
+    /// </summary>
+    /// <param name="unitToUse">the speedUnit to use</param>
+    public void setSpeedUnits(speedUnit unitToUse)
     {
-        
+        currentSpeedUnit = unitToUse;
+
+    }
+
+    /// <summary>
+    /// Sets the distance units to be used.
+    /// </summary>
+    /// <param name="unitToUse">The distanceUnit to use</param>
+    public void setDistanceUnits(distanceUnit unitToUse)
+    {
+        currentDistanceUnit = unitToUse;
+
     }
 }
---
+++ Unity Project/2019 Vertical Slice/Assets/SpeedDisplay.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpeedDisplay : MonoBehaviour
+{
+
+    OptionsManager _optionsManager;
+
+    public TextMeshProUGUI speedValue;
+    public TextMeshProUGUI speedUnits;
+
+    public GameObject sailboat;
+
+    Vector3 prevPos;
+    Vector3 currentPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //find the optionsManager to get the unit settings. If coming from menu, this should already exist and the one in-scene will destroy itself. Otherwise, it'll use the one in-scene.
+        _optionsManager = GameObject.FindObjectOfType<OptionsManager>();
+
+        //set the speed units display.
+        string units = "mph";
+        switch(_optionsManager.currentSpeedUnit)
+        {
+            case OptionsManager.speedUnit.KTS:
+                units = "kts";
+                break;
+            case OptionsManager.speedUnit.MPS:
+                units = "mps";
+                break;
+            default:
+                break;
+                
+        }
+
+        speedUnits.text = units;
+
+
+
+        prevPos = sailboat.transform.position;
+        currentPos = sailboat.transform.position;
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
