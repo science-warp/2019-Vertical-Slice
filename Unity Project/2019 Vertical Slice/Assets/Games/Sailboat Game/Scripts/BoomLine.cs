63dcb2c5c9103138c5afe976260144811b541838
Erin's Laptop <nern.strudel@gmail.com>
2019-08-01 20:31:02 -0400

    new stretchy boom line

--- Unity Project/2019 Vertical Slice/Assets/Games/Sailboat Game/Scripts/BoomLine.cs
+++ Unity Project/2019 Vertical Slice/Assets/Games/Sailboat Game/Scripts/BoomLine.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class BoomLine : MonoBehaviour
 {
 
-    public Transform leftAnchor;
-    public Transform rightAnchor;
+    public Transform bottomAnchor;
 
     public Transform boomAnchor;
 
@@ -16,12 +15,12 @@ public class BoomLine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        boomLineRenderer.SetPositions(new Vector3[] { leftAnchor.position, boomAnchor.position, rightAnchor.position });
+        boomLineRenderer.SetPositions(new Vector3[] { bottomAnchor.position, boomAnchor.position});
     }
 
     // Update is called once per frame
     void Update()
     {
-        boomLineRenderer.SetPositions(new Vector3[] {leftAnchor.position, boomAnchor.position, rightAnchor.position });
+        boomLineRenderer.SetPositions(new Vector3[] { bottomAnchor.position, boomAnchor.position});
     }
 }
